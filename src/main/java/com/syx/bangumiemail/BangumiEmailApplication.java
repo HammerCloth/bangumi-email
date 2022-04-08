2d63019397d865bef74211ef2dc73a9b6c89f60c
siyixiong <18552541076@163.com>
2022-04-08 21:24:08 +0800

    项目构建

---
+++ src/main/java/com/syx/bangumiemail/BangumiEmailApplication.java
@@ -0,0 +1,13 @@
+package com.syx.bangumiemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BangumiEmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BangumiEmailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/syx/bangumiemail/BangumiEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syx.bangumiemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BangumiEmailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
